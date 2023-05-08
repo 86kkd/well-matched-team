60052d2937f78b99233734b9d87575e7da426428
016 <meanful016@gmail.com>
2023-05-08 13:33:54 +0800

    服务器分支创建

--- Demo1-Server/src/main/java/com/versionone/demo1server/config/SpringMVCConfig.java
+++ Demo1-Server/src/main/java/com/versionone/demo1server/config/SpringMVCConfig.java
@@ -4,9 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SpringMVCConfig {
+public class SpringMVCConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
     @Bean
     public MultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
