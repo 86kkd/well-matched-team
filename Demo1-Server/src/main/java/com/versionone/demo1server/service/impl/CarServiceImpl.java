bdc292732f9bdd0b7f0b128326794221bf9fa83f
016 <meanful016@gmail.com>
2023-05-29 17:17:55 +0800

    后端：模拟平台的数据，并编写获取汽车信息接口

---
+++ Demo1-Server/src/main/java/com/versionone/demo1server/service/impl/CarServiceImpl.java
@@ -0,0 +1,20 @@
+package com.versionone.demo1server.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.versionone.demo1server.mapper.CarMapper;
+import com.versionone.demo1server.object.entity.Car;
+import com.versionone.demo1server.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarServiceImpl extends ServiceImpl<CarMapper, Car> implements CarService {
+
+    @Autowired
+    private CarMapper mapper;
+
+    @Override
+    public Car getCarInfoById(Integer id) {
+       return getById(id);
+    }
+}
