0bc8ed7d24e53e2b8b8279dc91d6ef008dbc1010
016 <meanful016@gmail.com>
2023-06-02 22:28:06 +0800

    修复合并bug

--- Demo1-Server/src/main/java/com/versionone/demo1server/service/impl/CarServiceImpl.java
+++ Demo1-Server/src/main/java/com/versionone/demo1server/service/impl/CarServiceImpl.java
@@ -22,10 +22,7 @@ public class CarServiceImpl extends ServiceImpl<CarMapper, Car> implements CarSe
         return getById(id);
     }
 
-    @Override
-    public Car getRandomCarInfo() {
-        return Redis.getRandomCar();
-    }
+
 
     @Override
     public Car brake(Integer id, Double time, Double strength) {
@@ -115,9 +112,7 @@ public class CarServiceImpl extends ServiceImpl<CarMapper, Car> implements CarSe
         return resultSpeed;
     }
 
-    private void updateCarOnList(Car car,Integer id){
-        Redis.updateCarInfoById(id, car);
-    }
+
 
     /**
      * 汽车刹车函数
@@ -138,41 +133,20 @@ public class CarServiceImpl extends ServiceImpl<CarMapper, Car> implements CarSe
         return resultSpeed;
     }
 
-    private Car getCarById(Integer id){
-        return Redis.getCarById(id);
-    }
+
 
     @Override
     public Car getRandomCarInfo() {
         return Redis.getRandomCar();
     }
 
-    @Override
-    public Car brake(Integer id, Double time, Double strength) {
-        if (strength<0 || strength>10 || id<=0){
-            return null;
-        }
-        Car car = getCarById(id);
-        Double speed = car.getSpeed();
-        Double resultSpeed = getBrakeResult(time,speed,strength);
-        car.setSpeed(resultSpeed);
-        saveOrUpdate(car);
-        updateCarOnList(car,id);
-        return car;
-    }
+
 
     private void updateCarOnList(Car car,Integer id){
         Redis.updateCarInfoById(id, car);
     }
 
-    private Double getBrakeResult(Double time,Double speed,Double strength){
-        double acceleration = ( 0.6 + (0.8-0.6) * RANDOM.nextDouble() )*strength ;
-        double resultSpeed  = speed - acceleration*(time/1000);
-        if (resultSpeed < 0){
-            return 0.0;
-        }
-        return resultSpeed;
-    }
+
 
     private Car getCarById(Integer id){
         return Redis.getCarById(id);
