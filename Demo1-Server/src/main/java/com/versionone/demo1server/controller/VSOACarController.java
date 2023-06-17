97b672d0e5adf8d4c11e5a3b5825c58ac6e5bf2a
016 <mr016@qq.com>
2023-06-17 17:24:07 +0800

    后端：设置汽车速度------VSOA通信控制中控平台接口编写

--- Demo1-Server/src/main/java/com/versionone/demo1server/controller/VSOACarController.java
+++ Demo1-Server/src/main/java/com/versionone/demo1server/controller/VSOACarController.java
@@ -1,8 +1,27 @@
 package com.versionone.demo1server.controller;
 
+import com.versionone.demo1server.service.VSOACarService;
+import com.versionone.demo1server.utils.CommonResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VSOACarController {
 
+    @Autowired
+    private VSOACarService service;
+
+    @RequestMapping(value = "/v_setSpeed" , method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult<String> setSpeed(@RequestParam("value")int speed){
+        try {
+            return CommonResult.success(service.setSpeed(speed));
+        } catch (Exception e) {
+            return CommonResult.failed("未知错误");
+        }
+    }
 }
